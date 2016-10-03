4cd3662fcd971fabb67a2b1f8e5c4066926315ad
Ubuntu <jillian.sue3@gmail.com>
2016-10-03 00:22:36 +0000

    hw

--- pytree.py
+++ pytree.py
@@ -3,50 +3,53 @@ import subprocess
 import sys
 import os
 
-arrow="├── "
-space="│   "
-lastArrow="└── "
-cdir=0
-cfile=0
+arrow = "├── "
+space = "│   "
+lastArrow = "└── "
+cdir = 0
+cfile = 0
+
 
 class element():
-    def __init__(self,name,path):
-        self.name=name 
-        self.path=path
+    def __init__(self, name, path):
+        self.name = name
+        self.path = path
         try:
-            self.dirList=os.listdir(path)
-            self.isdir=True
+            self.dirList = os.listdir(path)
+            self.isdir = True
         except:
-            self.dirList=[]
-            self.isdir=False
-        self.dirTree=[]
+            self.dirList = []
+            self.isdir = False
+        self.dirTree = []
+
 
 def getTree(directory):
-    global cdir,cfile
+    global cdir, cfile
     for d in directory.dirList:
-        e=element(d,os.path.join(directory.path,d))
+        e = element(d, os.path.join(directory.path, d))
         if e.isdir:
             getTree(e)
-            cdir+=1
+            cdir += 1
         else:
-            cfile+=1
+            cfile += 1
         directory.dirTree.append(e)
 
-def printTree(directory,prefixDefault=""):
-    for i in range(0,len(directory.dirTree)):
-        if i==len(directory.dirTree)-1:
-            prefix=prefixDefault+lastArrow
+
+def printTree(directory, prefixDefault = ""):
+    for i in range(0, len(directory.dirTree)):
+        if i == len(directory.dirTree) - 1:
+            prefix = prefixDefault + lastArrow
         else:
-            prefix=prefixDefault+arrow
-        e=directory.dirTree[i]
-        print (prefix+e.name)
+            prefix = prefixDefault + arrow
+        e = directory.dirTree[i]
+        print (prefix + e.name)
         if e.isdir:
-            printTree(e,prefixDefault+space)
-firstEl = element("",".")
+            printTree(e, prefixDefault + space)
+firstEl = element("", ".")
 getTree(firstEl)
-print (firstEl.path)
+print(firstEl.path)
 printTree(firstEl)
-print ("%d directories, %d files" % (cdir, cfile))
+print("%d directories, %d files" % (cdir, cfile))
 #if __name__ == '__main__':
-    # just for demo
-#    subprocess.run(['tree'] + sys.argv[1:])
+#just for demo
+#subprocess.run(['tree'] + sys.argv[1:])
